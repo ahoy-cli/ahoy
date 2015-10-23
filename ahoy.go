f151231d03da24636b2d7a1621b596690e7e10b3
Frank Carey <frank@devinci.io>
2015-10-23 02:14:57 -0400

    This doesn't work, but saving my place.

--- ahoy.go
+++ ahoy.go
@@ -5,13 +5,36 @@ import (
   "github.com/codegangsta/cli"
   "fmt"
   "os/exec"
+  "log"
+  "path"
 )
+
+func getComposeDir() string {
+  dir, err := os.Getwd()
+  if err != nil {
+    log.Fatal(err)
+  }
+  for dir != "" && err == nil {
+    dir, file := path.Split(dir)
+    cut_off_last_char_len := len(dir) - 1
+    dir = dir[:cut_off_last_char_len]
+    fmt.Println(dir)
+    _ = file
+  }
+  // Go complains with an error if you don't use a variable.
+  if err != nil {
+    log.Fatal(err)
+  }
+  return dir
+}
 
 func main() {
   app := cli.NewApp()
-  app.Name = "greet"
-  app.Usage = "fight the loneliness!"
+  app.Name = "ahoy"
+  app.Usage = "Send commands to docker-compose services"
   app.Action = func(c *cli.Context) {
+
+    fmt.Println(getComposeDir())
     cmd := exec.Command(os.Args[1], os.Args[2:]...)
     cmd.Stdout = os.Stdout
     cmd.Stdin = os.Stdin
