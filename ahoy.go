52403a03b93be053f8470ed810061cf109a45969
Aaron Couch <acinternets@gmail.com>
2016-01-13 15:41:26 -0600

    Update ahoy.go

--- ahoy.go
+++ ahoy.go
@@ -240,6 +240,12 @@ func main() {
 		if config.Usage != "" {
 			app.Usage = config.Usage
 		}
+		if config.Version != "" {
+			app.Version = config.Version
+		}
+		if config.Copyright != "" {
+			app.Copyright = config.Copyright
+		}
 	}
 
 	cli.AppHelpTemplate = `NAME:
