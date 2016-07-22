ee6504dd4cce682f1d81d0c8454c22173508a268
dkinzer <dtkinzer@gmail.com>
2016-07-22 09:49:03 -0300

    Refactor test.
    Remove unnecessary bits.

--- ahoy_test.go
+++ ahoy_test.go
@@ -2,28 +2,20 @@ package main
 
 import (
 	"github.com/codegangsta/cli"
-	"os"
 	"path/filepath"
 	"testing"
 )
 
 func TestOverrideExample(t *testing.T) {
-	os.Args = []string{"ahoy", "docker", "override-example"}
-	initFlags()
 	app = cli.NewApp()
 	app.Name = "ahoy"
-	overrideFlags(app)
 	if sourcefile, err := getConfigPath(sourcefile); err == nil {
 		sourcedir = filepath.Dir(sourcefile)
 		config, _ := getConfig(sourcefile)
 		app.Commands = getCommands(config)
-		app.Commands = addDefaultCommands(app.Commands)
-		if config.Usage != "" {
-			app.Usage = config.Usage
-		}
 	}
 
-	app.Run(os.Args)
+	app.Run([]string{"ahoy", "docker", "override-example"})
 	// Output
 	// "Overrode you."
 }
