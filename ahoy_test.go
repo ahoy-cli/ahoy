913678aedade14fa1dc1eb986bd7a62fcb6ad620
dkinzer <dtkinzer@gmail.com>
2016-07-22 12:02:58 -0300

    Capture output and check it in test.

--- ahoy_test.go
+++ ahoy_test.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 )
 
 func TestOverrideExample(t *testing.T) {
+	std_out := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
 	app = cli.NewApp()
 	app.Name = "ahoy"
+
 	if sourcefile, err := getConfigPath(sourcefile); err == nil {
 		sourcedir = filepath.Dir(sourcefile)
 		config, _ := getConfig(sourcefile)
@@ -16,6 +23,15 @@ func TestOverrideExample(t *testing.T) {
 	}
 
 	app.Run([]string{"ahoy", "docker", "override-example"})
-	// Output
-	// "Overrode you."
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = std_out
+
+	expected := "Overrode you.\n"
+	actual := string(out)
+
+	if expected != actual {
+		t.Errorf("ahoy docker override-example: expected - %s; actual - %s", string(expected), string(actual))
+	}
 }
